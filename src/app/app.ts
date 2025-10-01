ab07d2e940e13f7e81e58398e109ef00d36b0609
Estiven Correa <correaestiven09@gmail.com>
2025-10-01 07:44:56 -0600

    feat(lesson-02): Create home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home, Home as home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, Home],
+  templateUrl: './app.html', 
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('first-angular-app');
 }
+
