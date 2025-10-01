3a5b0c8bb3329ee8d2f7ad7e6eb5df2c70585fd9
Estiven Correa <correaestiven09@gmail.com>
2025-10-01 13:45:41 -0600

    feat(lesson-13): add search functionality

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -13,7 +13,20 @@ export class Home {
   housingLocationList: HousingLocationInfo[] = [];  
   housingService: HousingService = inject(HousingService);  
 
+  filteredLocationList: HousingLocationInfo[] = [];
+
   constructor() {    
-    this.housingLocationList = this.housingService.getAllHousingLocations();  
+    this.housingLocationList = this.housingService.getAllHousingLocations(); 
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
