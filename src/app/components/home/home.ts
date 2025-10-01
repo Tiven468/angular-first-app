4c9e3638312eb9c527376cc7b063036af460a235
Estiven Correa <correaestiven09@gmail.com>
2025-10-01 09:10:25 -0600

    feat(lesson-04): Interfaces

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
-
+import {HousingLocationInfo} from '../housinglocation';
 @Component({
   selector: 'app-home',
   imports: [HousingLocation],
@@ -8,5 +8,18 @@ import { HousingLocation } from '../housing-location/housing-location';
   styleUrl: './home.css'
 })
 export class Home {
+
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation:HousingLocationInfo = {
+    id: 999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: true,
+  }
 
 }
