2e4c4ad84190e7a39c97af2379da7fb82e8a9f9b
Estiven Correa <correaestiven09@gmail.com>
2025-10-01 13:32:15 -0600

    feat(lesson-12): integrate angular forms

--- src/app/components/details/details.ts
+++ src/app/components/details/details.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocationInfo } from '../housinglocation';
 import { CommonModule } from '@angular/common';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.html',
   styleUrls: ['./details.css']
 })
@@ -15,8 +16,22 @@ export class Details {
   housingService = inject(HousingService);
   housingLocation: HousingLocationInfo | undefined;
 
+ applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
